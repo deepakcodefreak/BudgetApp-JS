d1cf183431ca6699f64331eec77be35e70d535f3
deepakcodefreak <deepak19.engg@gmail.com>
2018-10-26 11:42:59 +0530

    event listener added for when enter key is pressed

--- app.js
+++ app.js
@@ -1,30 +1,49 @@
+//Data Controller
+
 var budgetController = (function(){
-  let a = 23;
-
-  var add  = function(x){
-    return x+a;
-  }
-
-  return {
-    publicTest:function(b){
-      return add(b);
-    }
-  }
-
+// Some code
 })();
 
+
+
+
+//UI Controller
 var UIController = (function(){
   // Some Code...
 })();
 
+
+
+
+//App Controller
 var Controller = (function(budgetCltr,UICltr){
 
+  var cltrAddItem = function(){
 
-    return {
-      anotherPublic:function(a){
-        var z = budgetCltr.publicTest(a);
-        return z;
+        // 1. Get the field input
+
+        // 2. Add the item to the budget Controller
+
+        //3. Add the item to the UI
+
+        //4. Calculate the budget
+
+        //5. Display the Budget on the UI
+          console.log('it works');
+  }
+
+  document.querySelector('.add__btn').addEventListener('click',cltrAddItem);
+
+
+  document.addEventListener('keypress',function(event){
+
+      if (event.keyCode === 13 || event.which === 13) {
+        cltrAddItem();
       }
-    }
+
+  })
+
+
+
 
 })(budgetController,UIController)
